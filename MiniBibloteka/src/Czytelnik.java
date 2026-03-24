f5440ff0eb7eee3f789471560b8a50567ad0dbe1
s33128 <s33128@pjwstk.edu.pl>
2026-03-24 21:27:07 +0100

    update klasy czytelnik

--- MiniBibloteka/src/Czytelnik.java
+++ MiniBibloteka/src/Czytelnik.java
@@ -1,52 +1,24 @@
 public class Czytelnik {
-    String imie;
-    String nazwisko;
-    String numerKarty;
-    int liczbaWypozyczen;
+    private String imie;
+    private String nazwisko;
+    private int numerKarty;
+    private int liczbaWypozyczen;
 
-    public String getImie() {
-        return imie;
-    }
-
-    public void setImie(String imie) {
-        this.imie = imie;
-    }
-
-    public String getNazwisko() {
-        return nazwisko;
-    }
-
-    public void setNazwisko(String nazwisko) {
-        this.nazwisko = nazwisko;
-    }
-
-    public String getNumerKarty() {
-        return numerKarty;
-    }
-
-    public void setNumerKarty(String numerKarty) {
-        this.numerKarty = numerKarty;
-    }
-
-    public int getLiczbaWypozyczen() {
-        return liczbaWypozyczen;
-    }
-
-    public void setLiczbaWypozyczen(int liczbaWypozyczen) {
-        this.liczbaWypozyczen = liczbaWypozyczen;
-    }
-
-    public Czytelnik(String imie, String nazwisko, String numerKarty, int liczbaWypozyczen) {
+    public Czytelnik(String imie, String nazwisko, int numerKarty) {
         this.imie = imie;
         this.nazwisko = nazwisko;
         this.numerKarty = numerKarty;
-        this.liczbaWypozyczen = liczbaWypozyczen;
+        this.liczbaWypozyczen = 0;
     }
-
-    public void wypiszdane(){
-        System.out.println(imie);
-        System.out.println(nazwisko);
-        System.out.println(numerKarty);
-        System.out.println(liczbaWypozyczen);
+    public void wypiszDane() {
+        System.out.println(imie + " " + nazwisko + " | Nr karty: " + numerKarty + " | Wypożyczenia: " + liczbaWypozyczen);
+    }
+    public void zwiekszLiczbeWypozyczen() {
+        liczbaWypozyczen++;
+    }
+    public void zmniejszLiczbeWypozyczen() {
+        if (liczbaWypozyczen > 0) {
+            liczbaWypozyczen--;
+        }
     }
 }
