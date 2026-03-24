e5ccd52b08f68ad1d33628f839ca852564f13d36
s33128 <s33128@pjwstk.edu.pl>
2026-03-24 21:25:10 +0100

    update klasy ksiazka

--- MiniBibloteka/src/Ksiazka.java
+++ MiniBibloteka/src/Ksiazka.java
@@ -1,76 +1,34 @@
-import java.util.Scanner;
-
 public class Ksiazka {
-    String tytul;
-    String autor;
-    int liczbaStron;
-    boolean czyDostepna;
-    int numerKsiazki;
+    private String tytul;
+    private String autor;
+    private int liczbaStron;
+    private boolean dostepna;
 
+    public Ksiazka(String tytul, String autor, int liczbaStron, boolean dostepna) {
+        this.tytul = tytul;
+        this.autor = autor;
+        this.liczbaStron = liczbaStron;
+        this.dostepna = dostepna;
+    }
+    public void wypiszInfo() {
+        System.out.println("Tytuł: " + tytul + ", Autor: " + autor + ", Strony: " + liczbaStron + ", Dostępna: " + (dostepna ? "Tak" : "Nie"));
+    }
+    public void wypozycz() {
+        if (dostepna) {
+            dostepna = false;
+            System.out.println("Wypożyczono książkę: " + tytul);
+        } else {
+            System.out.println("Książka niedostępna: " + tytul);
+        }
+    }
+    public void zwroc() {
+        dostepna = true;
+        System.out.println("Zwrócono książkę: " + tytul);
+    }
     public String getTytul() {
         return tytul;
     }
-
-    public void setTytul(String tytul) {
-        this.tytul = tytul;
-    }
-
-    public String getAutor() {
-        return autor;
-    }
-
-    public void setAutor(String autor) {
-        this.autor = autor;
-    }
-
-    public int getLiczbaStron() {
-        return liczbaStron;
-    }
-
-    public void setLiczbaStron(int liczbaStron) {
-        this.liczbaStron = liczbaStron;
-    }
-
-    public boolean isCzyDostepna() {
-        return czyDostepna;
-    }
-
-    public void setCzyDostepna(boolean czyDostepna) {
-        this.czyDostepna = czyDostepna;
-    }
-
-    public int getNumerKsiazki() {
-        return numerKsiazki;
-    }
-
-    public void setNumerKsiazki(int numerKsiazki) {
-        this.numerKsiazki = numerKsiazki;
-    }
-
-    public Ksiazka(String tytul, String autor, int liczbaStron, boolean czyDostepna, int numerKsiazki) {
-        this.tytul = tytul;
-        this.autor = autor;
-        this.liczbaStron = liczbaStron;
-        this.czyDostepna = czyDostepna;
-        this.numerKsiazki = numerKsiazki;
-    }
-
-    public void wypiszinfo() {
-        System.out.println(tytul);
-        System.out.println(autor);
-        System.out.println(liczbaStron);
-        System.out.println(czyDostepna);
-    }
-
-    public void wypozycz() {
-        if (czyDostepna == true) {
-            System.out.println("Na ile dni wypozyczasz ksiazke?");
-            Scanner sc = new Scanner(System.in);
-            int dni = sc.nextInt();
-            System.out.println("Wyporzyczono ksiazke na " + dni + " dni.");
-        } else {
-            System.out.println("Ksiazka jest niedotepna");
-        }
+    public boolean isDostepna() {
+        return dostepna;
     }
 }
-
