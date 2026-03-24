10464d505cae5fa4aec63777b19c65602ac0ddaa
s33128 <s33128@pjwstk.edu.pl>
2026-03-24 21:46:13 +0100

    klasa main

--- MiniBibloteka/src/Main.java
+++ MiniBibloteka/src/Main.java
@@ -2,5 +2,20 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Biblioteka biblioteka = new Biblioteka(10);
+        Ksiazka k1 = new Ksiazka("Wiedzmin", "Sapkowski", 300, true);
+        Ksiazka k2 = new Ksiazka("Hobbit", "Tolkien", 250, true);
+        Ksiazka k3 = new Ksiazka("1984", "Orwell", 200, true);
+        biblioteka.dodajKsiazke(k1);
+        biblioteka.dodajKsiazke(k2);
+        biblioteka.dodajKsiazke(k3);
+        Czytelnik c1 = new Czytelnik("Jan", "Kowalski", 1);
+        Czytelnik c2 = new Czytelnik("Anna", "Nowak", 2);
+        biblioteka.wypiszDostepneKsiazki();
+        biblioteka.wypozyczKsiazke("Hobbit", c1);
+        biblioteka.wypiszDostepneKsiazki();
+        biblioteka.zwrocKsiazke("Hobbit", c1);
+        biblioteka.wypiszDostepneKsiazki();
+        c1.wypiszDane();
     }
 }
